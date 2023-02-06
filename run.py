b1da0cec73d33d00a3f1ce696d69f3bf755cda99
BD-KING <106671590+Pavel-Mahmud-Alif@users.noreply.github.com>
2023-02-06 13:46:45 +0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://www.facebook.com/SH1N50')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import BAHA
+elif bit == '32bit':
+    print('w8 for update')
